9a6764f896ffec1e080b4e19676d9528978e7cb9
Avadhut-Parab_sudl <avadhut.parab@sudlife.in>
2026-05-04 07:17:57 +0530

    Add project files.

---
+++ dk-k8s-redhat-liux/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace dk_k8s_redhat_liux
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
